e9c0c85cdfd1da2dd55ac96daae1d41143fe558e
Noe Max Montiel Hernandez <nmontielh@Noes-MacBook-Air.local>
2021-08-12 21:43:21 -0500

    Agregar regla https

--- sg.tf
+++ sg.tf
@@ -11,6 +11,15 @@ resource "aws_security_group" "http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+    description = "HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port        = 0
     to_port          = 0
